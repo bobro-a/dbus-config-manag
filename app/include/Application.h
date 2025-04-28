0d84ced80dca0653d8085432e29fb3aa50416c3c
bobro-a <m3_ba@mail.ru>
2025-04-28 18:07:57 +0300

    добавила QTimer и обработчик таймера printData. Так же добавила private поляЮ для работы класса.

--- app/include/Application.h
+++ app/include/Application.h
@@ -1,16 +1,25 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 #include <QtDBus>
+#include <iostream>
 
 class Application : public QObject {
     Q_OBJECT
-    Application() {
-        loadStartData();
-    }
+
+public:
+    explicit Application(QObject *parent = nullptr);
+
 public slots:
     void newData();
-    void loadStartData();
+    void printData();
+
 private:
+    void loadStartData();
+
     QVariantMap data_;
+    QString filename_ = "confManagerApplication1";
+    QString pathDir_ = "com.system.configurationManager/Application";
+    QTimer timer_;
+    int currentTimeout;
 };
 #endif //APPLICATION_H
