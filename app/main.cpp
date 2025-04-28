a6e3875e173254ba0259b85bec80edfb771c1815
bobro-a <m3_ba@mail.ru>
2025-04-28 18:02:54 +0300

    подписалась на сигнал configurationChanged интерфейса сервиса и поставила для него slot обработчик newData приложения

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,14 @@
-#include <QCoreApplication>
-#include <QDBusConnection>
+#include "Application.h"
 
 int main(int argc,char* argv[]) {
     QCoreApplication app(argc,argv);
-
-    QDBusConnection conn=QDBusConnection::sessionBus().connect("com.system.configurationManager",
-        "/com/system/configurationManager/Application/confManagerApplication1",
+    Application application;
+    QDBusConnection conn=QDBusConnection::sessionBus();
+    conn.connect("com.system.configurationManager",
+        "com/system/configurationManager/Application/confManagerApplication1",
         "com.system.configurationManager.Application.Configuration",
         "configurationChanged",
-
+        &application, SLOT(newData(QVariantMap))
         );
 
     return app.exec();
